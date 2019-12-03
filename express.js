673988f1bec12e4d280481a6a832fc03f405e837
FacinouFabian <fabianfacinou02@gmail.com>
2019-12-03 17:30:36 +0100

    express GET to DELETE

--- express.js
+++ express.js
@@ -1,4 +1,8 @@
 const http = require('http')
+const local_database = './students.json'
+const file = require(local_database)
+const url = require('url')
+const fs = require('fs')
 class myExpress{
 
     constructor(){
@@ -13,9 +17,10 @@ class myExpress{
     get(path, callback){
         if (path === '/'){
             this.app.on('request', (req, res) => {
-                console.log(req.method)
+              if (req.method === 'GET'){
                 callback(req, res)
                 res.end()
+              }
             })
         }
     }
@@ -24,21 +29,130 @@ class myExpress{
         this.app.listen(port)
     }
 
-    // post(){
-    //     console.log('post')
-    // }
+    post(path, callback){
+      if (path === '/students'){
+        //1 get data
+        let body = ''
+          this.app.on('request', (req, res) => {
+            if (req.method === 'POST'){
+              req.on('data', chunk => {
+                  body += chunk.toString()
+              })
+              req.on('end', () => {
+                const user = JSON.parse(body)
+                const { name } = user
+                console.log(`New student ${name} inserted !`)
+                let data
+                if (!fs.existsSync(local_database)) {
+                    user.id = 1
+                    data = [user]
+                } else {
+                    const json = require(`./${local_database}`)
+                    user.id = json.length + 1
+                    json.push(user)
+                    data = json
+                }
+                fs.writeFileSync(local_database, JSON.stringify(data, null, 4))
+              })
+              callback(req, res)
+              res.end()
+            }
+          })
+      }
+    }
 
-    // put(){
-    //     console.log('put')
-    // }
 
-    // delete(){
-    //     console.log('delete')
-    // }
+    put(path, callback){
+      this.app.on('request', (req, res) => {
+        const {pathname, query} = url.parse(req.url, true)
+        const getID = pathname.split('/')
+        const studentID = getID[2]
+        if (pathname === `/students/${studentID}` && req.method === 'PUT'){
+          if (fs.existsSync(local_database)) {
+              //1 get data
+              let body = ''
 
-    // all(){
-    //     console.log('all')
-    // }
+              req.on('data', chunk => {
+                  body += chunk.toString()
+                  //postman
+                  const user = JSON.parse(body)
+                  const { name } = user
+                  for (const elt of file){
+                      if (elt.id === parseInt(studentID)){
+                          elt.name = name
+                      }
+                  }
+                  fs.writeFileSync(local_database, JSON.stringify(file))
+              })
+
+              req.on('end', () => {
+                  fs.writeFileSync(local_database, JSON.stringify(file, null, 4), function (err) {
+                      console.log(JSON.stringify(file))
+                  })
+              })
+          }
+          callback(req, res)
+          res.end()
+        }
+      })
+    }
+
+    delete(path, callback){
+      this.app.on('request', (req, res) => {
+        const {pathname, query} = url.parse(req.url, true)
+        const getID = pathname.split('/')
+        const studentID = getID[2]
+        const i = parseInt(studentID)
+        let index = 0
+        if (pathname === `/students/${studentID}` && req.method === 'DELETE'){
+          if (fs.existsSync(local_database)) {
+              for (const elt of file){
+                  if (elt.id === parseInt(studentID)){
+                    index = file.indexOf(elt)
+                      file.splice(index, index)
+                      console.log(file)
+                  }
+              }
+              fs.writeFileSync(local_database, JSON.stringify(file))
+
+              req.on('end', () => {
+                  fs.writeFileSync(local_database, JSON.stringify(file, null, 4), function (err) {
+                      console.log(JSON.stringify(file))
+                  })
+              })
+          }
+          callback(req, res)
+          res.end()
+        }
+      })
+    }
+
+    /*all(path, callback){
+      this.app.on('request', (req, res) => {
+        const {pathname, query} = url.parse(req.url, true)
+        //GET
+        if (pathname === '/' && req.method === 'GET'){
+          //this.get(path, callback)
+          console.log('GET')
+        }
+        //POST
+        else if (pathname === '/students' && req.method === 'POST'){
+          // this.post(path, callback)
+          console.log('POST')
+        }
+        //PUT
+        else if (pathname === '/students/' && req.method === 'PUT'){
+          // this.put(path, callback)
+          console.log('PUT')
+        }
+        //DELETE
+        else if (pathname === '/students/' && req.method === 'DELETE'){
+          // this.delete(path, callback)
+          console.log('DELETE')
+        }
+        res.end()
+      })
+    }*/
 
     // render(){
     //     console.log('render')
--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 const express = require('./express')
 const app = express()
 
-
 app.get('/', function (req, res) {
     res.write('GET request to homepage')
 })
 
+app.post('/students', function (req, res) {
+    res.write('POST request to homepage')
+})
+
+app.put('/students/', function (req, res) {
+    res.write('PUT request to homepage')
+})
+
+app.delete('/students/', function (req, res) {
+    res.write('DELETE request to homepage')
+})
+
+// app.all('/*', function (req, res) {
+//       res.write('Request to homepage')
+//       next()
+// })
+
 app.listen(4242)
 
 
-// app.post()
 
-// app.delete()
-
-// app.put()
-
-// app.all()
-
-// app.render() 
+// app.render()
